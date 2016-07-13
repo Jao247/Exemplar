6ca18ea442b9cf683c4a6cdfc870cbb7548ae007
Jason-Fahy <Jason-Fahy@CGADEV008>
2016-07-13 09:57:18 +0100

    Add project files.

---
+++ WritingToFile/RootViewController.swift
@@ -0,0 +1,27 @@
+﻿import UIKit
+
+@IBObject class RootViewController : UIViewController {
+
+	let groupIdentity = "group.jason.tester";
+	let fileName = "example.text";
+	@IBOutlet weak var eText: UITextField;
+
+	@IBAction func writeAction(from: AnyObject)
+	{
+		let alertView: UIAlertView = UIAlertView(title: "Title", message: "Message", delegate: self, cancelButtonTitle: "OK", otherButtonTitles: nil);
+		alertView.show();
+	}
+
+	public override func viewDidLoad() {
+
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	public override func didReceiveMemoryWarning() {
+
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+}
