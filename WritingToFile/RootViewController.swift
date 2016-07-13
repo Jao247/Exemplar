55c1bc5016eb5f80b90df49dca2ee6aab02759b1
Jao247 <jason.fahy@cgatechnology.com>
2016-07-13 13:11:03 +0100

    added functionality

--- WritingToFile/RootViewController.swift
+++ WritingToFile/RootViewController.swift
@@ -3,14 +3,17 @@
 @IBObject class RootViewController : UIViewController {
 
     let groupIdentity = "group.jason.tester";
-    let fileName = "example.txt";
+    let fileName = "/example.txt";
 
     @IBOutlet weak var eText: UITextField;
 
     @IBAction func alertMsg(sender: AnyObject)
     {
-	let alert = UIAlertView(title: "Title", message: eText.text, delegate: self, cancelButtonTitle: "Done", otherButtonTitles: nil);
-	alert.show();
+	let dir = containerURLForSecurityApplicationGroupIdentifier(groupIdentity);
+        let fileMgr = NSFileManager.defaultmanager();
+
+        try! fileMgr.createFileAtPath(dir.absoluteString + fileName, contents: eText.text, attributes: nil);
+        let alert = UIAlertView(title: "Accomplished", message: "\"\(eText.text)\"\nHas been written to a file.", delegate: self, cancelButtonTitle: "Done", otherButtonTitles: nil);
     }
 
     public override func viewDidLoad() 
