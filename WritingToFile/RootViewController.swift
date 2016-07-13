207c2762b4e998d1c9e6a1d2742123d93cb1aa33
Jao247 <jason.fahy@cgatechnology.com>
2016-07-13 11:52:49 +0100

    a list

--- WritingToFile/RootViewController.swift
+++ WritingToFile/RootViewController.swift
@@ -4,22 +4,15 @@
 
 	let groupIdentity = "group.jason.tester";
 	let fileName = "example.txt";
-	@IBOutlet weak var eText: UITextField;
 
-	@IBAction func writeAction(from: AnyObject)
+	public override func viewDidLoad() 
 	{
-		let alertView: UIAlertView = UIAlertView(title: "Title", message: eText.text, delegate: self, cancelButtonTitle: "OK", otherButtonTitles: nil);
-		alertView.show();
-	}
-
-	public override func viewDidLoad() {
-
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
-	public override func didReceiveMemoryWarning() {
-
+	public override func didReceiveMemoryWarning() 
+	{
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
