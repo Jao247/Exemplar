195fa0d78609a791cb2a0ee409e2d0789b698095
Jao247 <jason.fahy@cgatechnology.com>
2016-07-13 13:00:12 +0100

    altered

--- WritingToFile/RootViewController.swift
+++ WritingToFile/RootViewController.swift
@@ -2,27 +2,24 @@
 
 @IBObject class RootViewController : UIViewController {
 
-	let groupIdentity = "group.jason.tester";
-	let fileName = "example.txt";
+    let groupIdentity = "group.jason.tester";
+    let fileName = "example.txt";
 
-	@IBOutlet weak var eText: UITextField;
+    @IBOutlet weak var eText: UITextField;
 
-	@IBAction func alertMsg(sender: AnyObject)
-	{
-		let alert = UIAlertView(title: "Title", message: eText.text, delegate: self, cancelButtonTitle: "Done", otherButtonTitles: nil);
-		alert.show();
-	}
+    @IBAction func alertMsg(sender: AnyObject)
+    {
+	let alert = UIAlertView(title: "Title", message: eText.text, delegate: self, cancelButtonTitle: "Done", otherButtonTitles: nil);
+	alert.show();
+    }
 
-        
+    public override func viewDidLoad() 
+    {
+	super.viewDidLoad()
+    }
 
-	public override func viewDidLoad() 
-	{
-		super.viewDidLoad()
-	}
-
-	public override func didReceiveMemoryWarning() 
-	{
-		super.didReceiveMemoryWarning()
-	}
-
+    public override func didReceiveMemoryWarning() 
+    {
+	super.didReceiveMemoryWarning()
+    }
 }
