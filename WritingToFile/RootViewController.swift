f3c199c97f51110f1407f23a598f1e9be655280f
Jao247 <jason.fahy@cgatechnology.com>
2016-07-13 12:24:38 +0100

    A CommitAll

--- WritingToFile/RootViewController.swift
+++ WritingToFile/RootViewController.swift
@@ -15,13 +15,11 @@
 	public override func viewDidLoad() 
 	{
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 
 	public override func didReceiveMemoryWarning() 
 	{
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
 }
