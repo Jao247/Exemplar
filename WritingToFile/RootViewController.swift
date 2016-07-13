a0c1a8f2da9bf9be98308d5c1a78515b9c0b3736
Jao247 <jason.fahy@cgatechnology.com>
2016-07-13 12:04:54 +0100

    This tests

--- WritingToFile/RootViewController.swift
+++ WritingToFile/RootViewController.swift
@@ -5,6 +5,13 @@
 	let groupIdentity = "group.jason.tester";
 	let fileName = "example.txt";
 
+	@IBOutlet weak var eText: UITextField;
+
+	@IBAction func writeData()
+	{
+
+	}
+
 	public override func viewDidLoad() 
 	{
 		super.viewDidLoad()
